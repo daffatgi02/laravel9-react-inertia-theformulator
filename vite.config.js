dc6daae87517a964ff03b14d7054ce47af9a8ea9
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2025-08-04 13:47:39 +0700

    tes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    laravel(['resources/css/app.css', 'resources/js/app.js']),
-    tailwindcss(),
-  ],
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
